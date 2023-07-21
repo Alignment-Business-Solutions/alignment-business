3ac2d77b4420e0298067e700b2a22d96fe25815c
Will Richardson <rich1051@umn.edu>
2023-07-21 11:56:20 -0500

    Changed Favicon, updated AboutPage text, updated nav bar for better usability, changed footer to have company name

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -9,7 +9,49 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>
+          Introducing "ExpenseTrackerPro" by Alignment Business Solutions, a
+          powerful application designed to help both Alignment and its valued
+          clients monitor and manage their expenses seamlessly. At Alignment, we
+          understand that excellent customer support and efficient financial
+          operations are the pillars of success for any business. With
+          ExpenseTrackerPro, we extend our commitment to comprehensive business
+          and client success management services, providing a user-friendly and
+          reliable solution to handle expenses effectively.
+        </p>
+        <p>
+          ExpenseTrackerPro empowers businesses with a centralized platform to
+          track and analyze expenses effortlessly. Our application offers a
+          range of features, including expense categorization, real-time expense
+          tracking, and customizable reporting, allowing clients to gain
+          valuable insights into their financial activities. With a focus on
+          accuracy and timeliness, our expert team at Alignment Business
+          Solutions ensures that all expense-related data is up-to-date and
+          error-free, fostering trust and transparency between businesses and
+          their clients.
+        </p>
+        <p>
+          In addition to managing expenses, ExpenseTrackerPro also streamlines
+          the billing and invoicing process for Alignment's clients. The
+          application automates invoicing, generates professional-looking
+          invoices, and sends them to clients promptly, eliminating manual
+          efforts and reducing the risk of errors. Furthermore, clients can
+          access a secure client portal within the application, where they can
+          view their invoices, payment history, and other financial information.
+          This fosters a sense of community, as both Alignment and its clients
+          collaborate closely to achieve mutual success.
+        </p>
+        <p>
+          At Alignment Business Solutions, we are dedicated to empowering our
+          clients to achieve their business goals. ExpenseTrackerPro serves as a
+          testament to our belief in the power of community, enabling businesses
+          to thrive together with top-notch customer support, streamlined
+          billing and invoicing, and seamless expense monitoring. With
+          ExpenseTrackerPro, you can focus on providing exceptional support to
+          your clients while leaving the financial management in capable hands.
+          Together, we can achieve more, ensuring sustained growth and
+          prosperity for every business we serve.
+        </p>
       </div>
     </div>
   );
