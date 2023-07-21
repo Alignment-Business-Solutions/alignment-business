fad128d0f7c450e23c55c57f120b6ad18aae0e40
Majid Igale <majidigale18@gmail.com>
2023-07-21 12:41:10 -0500

    added another Get for all cleints

--- server/routes/client_management.router.js
+++ server/routes/client_management.router.js
@@ -9,6 +9,19 @@ router.get('/', (req,res) => {
     ON client.accountant_id = accountants.id
    WHERE accountant_id = $1;` , [req.user.id])
     .then(result => {
+        console.log('clientRouter GET result by Accountant ==> ', result.rows)
+        res.send(result.rows)
+    }).catch( err => {
+        console.log('Error with ClientRouter with Accountant GET', err)
+        res.sendStatus(500)
+    })
+
+});
+
+router.get('/', (req,res) => {
+    // GET route code here
+    pool.query(`SELECT * FROM client` , [req.user.id])
+    .then(result => {
         console.log('clientRouter GET result ==> ', result.rows)
         res.send(result.rows)
     }).catch( err => {
