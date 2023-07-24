833513ae2b16b4c823db25be4932a0675303d7b9
Majid Igale <majidigale18@gmail.com>
2023-07-24 15:40:20 -0500

    imported dependcies for MyClients component

--- src/components/MyClients/MyClients.jsx
+++ src/components/MyClients/MyClients.jsx
@@ -1,5 +1,11 @@
+import React, { useEffect, useReact } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
 
 function MyClients() {
+
+
 
         return (
             <p>Under Construction</p>
