5f95dff1d1051e8b5b48bc833148ce4c7e47a984
Majid Igale <majidigale18@gmail.com>
2023-07-24 09:51:30 -0500

    created put to dispatch to clients reducer and send out the server data from the DB

--- src/redux/sagas/clients.saga.js
+++ src/redux/sagas/clients.saga.js
@@ -6,7 +6,8 @@ function* fetchMyClients() {
         const clients = yield axios.get('/api/client')
         console.log('This is GET results coming from the client Server', clients.data)
         yield put({
-            type: ''
+            type: 'SET_MY_CLIENTS',
+            payload: clients.data
         })
     } catch(err) {
         console.log('Error with MyCLients SAGA ', err)
