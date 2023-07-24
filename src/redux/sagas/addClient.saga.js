4ceff5f14dc0e893da837c635b465dfd1e1f5391
Majid Igale <majidigale18@gmail.com>
2023-07-24 14:11:21 -0500

    finished addClient saga logic

--- src/redux/sagas/addClient.saga.js
+++ src/redux/sagas/addClient.saga.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 import {put, takeLatest} from 'redux-saga/effects';
 
 
-function* addClient() {
+function* addClient(action) {
     try{
-
+        console.log('PUT Add Client action.payload ===>', action.payload)
+        yield axios.put('/api/client/add', action.payload)
+        yield ({
+            type: 'FETCH_MY_CLIENTS'
+        })
     } catch(err) {
         console.log('Error with Add Client PUT saga', err)
     }
