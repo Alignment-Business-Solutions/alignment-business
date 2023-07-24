4bda43aabcdeb4a6e78f9745ab83029185f84410
Majid Igale <majidigale18@gmail.com>
2023-07-24 14:08:19 -0500

    created generator function for addClient saga

--- src/redux/sagas/addClient.saga.js
+++ src/redux/sagas/addClient.saga.js
@@ -1,2 +1,11 @@
 import axios from 'axios'
 import {put, takeLatest} from 'redux-saga/effects';
+
+
+function* addClient() {
+    try{
+
+    } catch(err) {
+        console.log('Error with Add Client PUT saga', err)
+    }
+}
