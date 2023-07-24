afd06ed0120aefdede062144aef837ff52613e5e
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-07-24 10:10:11 -0500

    started axios request in spinglePL saga to fetch data from server

--- server/routes/singlePL.router.js
+++ server/routes/singlePL.router.js
@@ -2,11 +2,14 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
 router.get('/', (req, res) => {
-  // GET route code here
+    
+
+
+
+
+
 });
 
 /**
--- src/redux/sagas/singlePL.saga.js
+++ src/redux/sagas/singlePL.saga.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest, takeLeading } from 'redux-saga/effects';
 
 
 
-function* festWeekData() {
+function* fetchWeekData() {
     
     try {
-        const results = yield axios.get('/api/
-
+        const results = yield axios.get('/api/single');
+        yield put({type:"SET_WEEK_DATA", payload:results.data});
     }
     catch {
-
-
+        console.log('error with fetching week data from  server');
     }
 }
 
@@ -20,7 +19,7 @@ function* festWeekData() {
 
 
 function* singlePLSaga() {
-
+    yield takeLatest("FETCH_WEEK", fetchWeekData);
 
 
 
