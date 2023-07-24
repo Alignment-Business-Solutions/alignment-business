e52a7367a8827f54065623e7210f1906d4a811ae
Majid Igale <majidigale18@gmail.com>
2023-07-24 11:56:16 -0500

    created generator function named fetchMyclients and wrote logic for it

--- src/redux/sagas/myClients.saga.js
+++ src/redux/sagas/myClients.saga.js
@@ -1,2 +1,15 @@
 import axios from 'axios'
-import {put, takeLatest}
+import {put, takeLatest} from 'redux-saga/effects'
+
+function* fetchMyClients() {
+    try {
+        const myClients = yield axios.get('/api/client/myClients')
+        console.log('This is GET results coming from the myClients request', myClients.data)
+        yield put({
+            type: 'SET_MY_CLIENTS',
+            payload: myClients.data
+        })
+    } catch(err) {
+        console.log('Error with MyCLients SAGA ', err)
+    }
+}
