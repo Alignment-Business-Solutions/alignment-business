4c4fb8e1a67ba0c2c903c6440952b4b83df4e88f
Majid Igale <majidigale18@gmail.com>
2023-07-24 09:47:45 -0500

    created path for saga to grab server Data from client.management route

--- src/redux/sagas/clients.saga.js
+++ src/redux/sagas/clients.saga.js
@@ -3,7 +3,11 @@ import {put, takeLatest} from 'redux-saga/effects'
 
 function* fetchMyClients() {
     try {
-
+        const clients = yield axios.get('/api/client')
+        console.log('This is GET results coming from the client Server', clients.data)
+        yield put({
+            type: ''
+        })
     } catch(err) {
         console.log('Error with MyCLients SAGA ', err)
     }
