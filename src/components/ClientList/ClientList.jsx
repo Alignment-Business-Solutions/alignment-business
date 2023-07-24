d847d9dd24baa3871bf16a33d8f28f2eb7f10e0e
Majid Igale <majidigale18@gmail.com>
2023-07-24 10:35:57 -0500

    created dispatch for Fetch Clients and imported my store into compoent. Lastly, changed myclients Reducer to a lower case m

--- src/components/ClientList/ClientList.jsx
+++ src/components/ClientList/ClientList.jsx
@@ -2,10 +2,22 @@ import React,{useEffect, useReact} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 
 function ClientList() {
+    const dispatch = useDispatch();
+    const myClients = useSelector((store) => store.myClients)
+
+    useEffect(() => {
+        dispatch({
+          type: "FETCH_MY_CLIENTS",
+        });
+      }, []);
+
 
     return (
+        <div>
         <h1>Under Construction</h1>
-    )
+
+   </div> 
+   )
 }
 
 export default ClientList
--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import MyClients from './clients.reducer';
+import myClients from './clients.reducer';
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -11,7 +11,7 @@ import MyClients from './clients.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  MyClients
+  myClients
 });
 
 export default rootReducer;
