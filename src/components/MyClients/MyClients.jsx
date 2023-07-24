f2e1192bf52c19c45424586bed22a352b72281cb
Majid Igale <majidigale18@gmail.com>
2023-07-24 15:45:21 -0500

    imported myclients reducer in root reducer

--- src/components/MyClients/MyClients.jsx
+++ src/components/MyClients/MyClients.jsx
@@ -4,9 +4,15 @@ import { useHistory } from "react-router-dom";
 
 
 function MyClients() {
-    
-    const dispatch = useDispatch();
 
+    const dispatch = useDispatch();
+    const allClients = useSelector((store) => store.allClients);
+
+  useEffect(() => {
+    dispatch({
+      type: "FETCH_MY_CLIENTS",
+    });
+  }, []);
 
         return (
             <p>Under Construction</p>
