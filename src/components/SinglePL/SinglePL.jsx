d9dc25f9e680454abb49da92bbfcdd15ca2ba112
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-07-24 09:56:06 -0500

    created the route to use single page

--- src/components/SinglePL/SinglePL.jsx
+++ src/components/SinglePL/SinglePL.jsx
@@ -1,4 +1,33 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+
+function SinglePL() {
+
+    const dispatch = useDispatch();
+    const weekData = useSelector(store => store.singlePLReducer);
+
+
+    useEffect(() => {
+    
+    }, []);
+
+
+
+
+
+
+    return (
+        <p>as;dlkfjas;ldkfjfa;sldkfjl</p>
+    );
+
+
+
+
+}
+
+
+export default SinglePL;
 
 
 
