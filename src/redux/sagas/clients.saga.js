029dc0ac02e684dd99c46bd2d951d794f2bf7100
Majid Igale <majidigale18@gmail.com>
2023-07-24 09:44:40 -0500

    created fetch function for clients saga

--- src/redux/sagas/clients.saga.js
+++ src/redux/sagas/clients.saga.js
@@ -1,2 +1,10 @@
 import axios from 'axios'
 import {put, takeLatest} from 'redux-saga/effects'
+
+function* fetchMyClients() {
+    try {
+
+    } catch(err) {
+        console.log('Error with MyCLients SAGA ', err)
+    }
+}
