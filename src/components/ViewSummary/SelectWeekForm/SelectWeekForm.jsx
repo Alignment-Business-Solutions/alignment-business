c399c2bbafa21993cbea08b593f86527d3a6814e
Adam Oberle <oberle.adam@gmail.com>
2023-07-25 10:42:43 -0500

    refactored viewsummary with new components for SelectWeek Form, RecentPLTable, and AddWeekForm

---
+++ src/components/ViewSummary/SelectWeekForm/SelectWeekForm.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+
+
+function SelectWeekForm() {
+
+    const dispatch = useDispatch();
+
+    const weeksDropdown = useSelector(store => store.weeksDropdown)
+    const [weekSelected, setWeekSelected] = useState(0)
+
+    function goToWeek(event) {
+        event.preventDefault();
+        dispatch({ type: 'FETCH_WEEK', payload: weekSelected})
+    }
+
+    return (
+        <div>
+            <form onSubmit={(event) => goToWeek(event)}>
+                <select
+                    value={weekSelected}
+                    onChange={(event) => setWeekSelected(event.target.value)}
+                >
+                    <option value=""> -- Click to Select a week!</option>
+                    {weeksDropdown.map(week => (
+                        <option key={week.id} value={week.id}>{week.start_date}</option>
+                    ))}
+                </select>
+                <button type="submit">Go To Week Details</button>
+            </form>
+        </div>
+    )
+}
+export default SelectWeekForm;
