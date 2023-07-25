26b99c7b9f45ecc66baa43340f7f8f43650f73dd
Majid Igale <majidigale18@gmail.com>
2023-07-25 10:14:22 -0500

    created my client remove PUT request on router

--- server/routes/client_management.router.js
+++ server/routes/client_management.router.js
@@ -51,15 +51,31 @@ router.put('/add', (req, res) => {
         res.sendStatus(500)
       })
       
-    //   .then(result => {
-    //     res.sendStatus(201)
-    //   })
-    //   .catch(err => {
-    //     console.log('Error SERVER UPDATING CLIENT LIST===>', err)
-    //     res.sendStatus(500)
-    //   })
+  
 
 });
+
+router.put('/remove', (req, res) => {
+    console.log('OUR REQ.BODY',req.body)
+      // PUT route code here
+      // protect route at some point
+     const sqlText = `UPDATE client SET "accountant_id" = null
+      WHERE client."id" = $1;
+      `;
+    const sqlValues = [req.user.id,req.body.client_id]
+      pool.query(sqlText,sqlValues)
+      .then(result => {
+        res.sendStatus(201)
+      }).catch(err => {
+        console.log('Error with CLIENT PUT', err)
+        res.sendStatus(500)
+      })
+      
+  
+
+});
+//UPDATE client SET "accountant_id" = null 
+//WHERE client.id = 1;
 
 router.delete('/', (req, res) => {
     // DELETE route code here
