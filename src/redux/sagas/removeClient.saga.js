f1430c8e9113905217683cb153c87ae492aa8c11
Majid Igale <majidigale18@gmail.com>
2023-07-25 10:16:27 -0500

    created remove egenerator function with logic on remove saga

--- src/redux/sagas/removeClient.saga.js
+++ src/redux/sagas/removeClient.saga.js
@@ -2,3 +2,14 @@ import axios from 'axios'
 import {put, takeLatest} from 'redux-saga/effects';
 
 
+function* removeClient(action) {
+    try{
+        console.log('PUT REMOVE Client action.payload ===>', action.payload)
+        yield axios.put('/api/client/remove', action.payload)
+        yield ({
+            type: 'FETCH_MY_CLIENTS'
+        })
+    } catch(err) {
+        console.log('Error with Add Client PUT saga', err)
+    }
+}
