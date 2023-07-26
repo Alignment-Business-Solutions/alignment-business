55eb69e60c2705e8ffe6b63b458685f72cee753d
Majid Igale <majidigale18@gmail.com>
2023-07-26 15:42:01 -0500

    created html table with header titles

--- src/components/BalanceSheet/BalanceSheet.jsx
+++ src/components/BalanceSheet/BalanceSheet.jsx
@@ -3,8 +3,29 @@
 function BalanceSheet(){
 
     return (
+        <>
         <p> Under Construction </p>
-    )
+        
+        <table id='table'>
+  <thead>
+    <tr>
+      <th >Beginning Cash</th>
+      <th >Income (received)</th>
+      <th >Expenses (paid)</th>
+      <th >Expenses (expected)</th>
+      <th >Expenses (expected)</th>
+      <th>To/(From) Savings</th>
+      <th>Saving Balance</th>
+      <th>Outstanding Checks</th>
+      <th>Loan (To)/From</th>
+      <th>Ending Balance (cleared)</th>
+      <th>Ending Balance (actual)</th>
+    </tr>
+  </thead>
+  <tbody>
+</tbody>
+</table>
+   </> )
 }
 
 export default BalanceSheet
