8546da8e48a4cc80649e1e690ebb5cf86bf2d358
Majid Igale <majidigale18@gmail.com>
2023-07-26 13:57:12 -0500

    created some blank http request in router file

--- server/routes/balance.router.js
+++ server/routes/balance.router.js
@@ -2,18 +2,23 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
 router.get('/', (req, res) => {
   // GET route code here
 });
 
-/**
- * POST route template
- */
+
+router.put('/', (req, res) => {
+    // PUT route code here
+  });
+  
+
 router.post('/', (req, res) => {
   // POST route code here
 });
 
+router.delete('/', (req, res) => {
+    // DELETE route code here
+  });
+  
 module.exports = router;
