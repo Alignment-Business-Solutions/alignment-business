06649145b0b576e53112d7bacba3af7243eca7c6
Majid Igale <majidigale18@gmail.com>
2023-07-26 14:24:42 -0500

    created generator function that fetches the get request from the router

--- src/redux/sagas/fetchBalance.saga.js
+++ src/redux/sagas/fetchBalance.saga.js
@@ -1,2 +1,15 @@
 import axios from 'axios'
 import {put, takeLatest} from 'redux-saga/effects'
+
+function* fetchBalance() {
+    try {
+        const balance = yield axios.get('/api/balance')
+        console.log('This is GET results coming from the Balance Server', balance.data)
+        yield put({
+            type: 'SET_BALANCE',
+            payload: balance.data
+        })
+    } catch(err) {
+        console.log('Error with Balance SAGA ', err)
+    }
+}
