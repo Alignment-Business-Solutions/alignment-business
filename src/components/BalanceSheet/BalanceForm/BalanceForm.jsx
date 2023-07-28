a97180da501b0d32bacbf56d348a428c695d0229
Majid Igale <majidigale18@gmail.com>
2023-07-28 11:59:16 -0500

    finished creating input logic

--- src/components/BalanceSheet/BalanceForm/BalanceForm.jsx
+++ src/components/BalanceSheet/BalanceForm/BalanceForm.jsx
@@ -22,13 +22,29 @@ function BalanceForm() {
 
     let [balance, setBalance] = useState(totalBalance)
 
+    // const handleCreateBalance = (event) => {
+    //     event.preventDefault()
+    //     console.log('Created a New Balance')
+
+    // }
+
     return (
         <form>
+                <label>New Week</label>
+                        <input 
+                        type='date'
+                        placeholder='Start Date'
+                        value={balance.start_date}
+                        onChange={(e) => setBalance({
+                           ...totalBalance,
+                           start_date: e.target.value
+                        })} 
+                        />
              <label>Beginning Cash</label>
                         <input 
-                        type='number'
+                        type='text'
                         placeholder='Beginning Cash'
-                        value={balance}
+                        value={balance.beginning_cash}
                         onChange={(e) => setBalance({
                            ...totalBalance,
                            beginning_cash: e.target.value
@@ -36,9 +52,9 @@ function BalanceForm() {
                         />
                          <label>Income Received</label>
                         <input 
-                        type='number'
+                        type='text'
                         placeholder='Income Received'
-                        value={balance}
+                        value={balance.income_received}
                         onChange={(e) => setBalance({
                            ...totalBalance,
                            income_received: e.target.value
@@ -46,9 +62,9 @@ function BalanceForm() {
                         />
                           <label>Expenses Paid</label>
                         <input 
-                        type='number'
+                        type='text'
                         placeholder='Expenses Paid'
-                        value={balance}
+                        value={balance.expenses_paid}
                         onChange={(e) => setBalance({
                            ...totalBalance,
                            expenses_paid: e.target.value
@@ -56,9 +72,9 @@ function BalanceForm() {
                         />
                             <label>Expenses Expected</label>
                         <input 
-                        type='number'
+                        type='text'
                         placeholder='Expenses Expected'
-                        value={balance}
+                        value={balance.expenses_expected}
                         onChange={(e) => setBalance({
                            ...totalBalance,
                            expenses_expected: e.target.value
@@ -66,24 +82,65 @@ function BalanceForm() {
                         />
                             <label>To/(From) Savings</label>
                         <input 
-                        type='number'
+                        type='text'
                         placeholder='To/(From) Savings'
-                        value={balance}
+                        value={balance.to_from_savings}
                         onChange={(e) => setBalance({
                            ...totalBalance,
                            to_from_savings: e.target.value
                         })} 
                         />
-                                <label>To/(From) Savings</label>
+                                <label>Saving Balance</label>
                         <input 
-                        type='number'
-                        placeholder='To/(From) Savings'
-                        value={balance}
+                        type='text'
+                        placeholder='Saving Balance'
+                        value={balance.saving_balance}
                         onChange={(e) => setBalance({
                            ...totalBalance,
                            saving_balance: e.target.value
                         })} 
                         />
+                                  <label>Outstanding Checks</label>
+                        <input 
+                        type='text'
+                        placeholder='Outstanding Checks'
+                        value={balance.outstanding_checks}
+                        onChange={(e) => setBalance({
+                           ...totalBalance,
+                           outstanding_checks: e.target.value
+                        })} 
+                        />
+                                  <label>Loan (To)/From</label>
+                        <input 
+                        type='text'
+                        placeholder='Loan (To)/From'
+                        value={balance.loan_to_from}
+                        onChange={(e) => setBalance({
+                           ...totalBalance,
+                           loan_to_from: e.target.value
+                        })} 
+                        />
+                                  <label>Ending Balance (cleared)</label>
+                        <input 
+                        type='text'
+                        placeholder='Ending Balance (cleared)'
+                        value={balance.ending_balance_cleared}
+                        onChange={(e) => setBalance({
+                           ...totalBalance,
+                           ending_balance_cleared: e.target.value
+                        })} 
+                        />
+                                  <label>Ending Balance (actual)</label>
+                        <input 
+                        type='text'
+                        placeholder='Ending Balance (actual)'
+                        value={balance.ending_balance_actual}
+                        onChange={(e) => setBalance({
+                           ...totalBalance,
+                           ending_balance_actual: e.target.value
+                        })} 
+                        />
+             
         </form>
     )
 }
