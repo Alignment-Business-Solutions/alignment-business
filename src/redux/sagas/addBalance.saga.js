74235cf4c6a46ce362be87816a71158b58748bf4
Majid Igale <majidigale18@gmail.com>
2023-07-28 10:57:43 -0500

    added balance generator function

--- src/redux/sagas/addBalance.saga.js
+++ src/redux/sagas/addBalance.saga.js
@@ -1,2 +1,16 @@
 import axios from 'axios'
 import {put, takeLatest} from 'redux-saga/effects';
+
+
+
+function* addBalance(action) {
+    try{
+        console.log('POST Add Balance action.payload ===>', action.payload)
+        yield axios.put('/api/balance', action.payload)
+        yield put({
+            type: 'FETCH_BALANCE'
+        })
+    } catch(err) {
+        console.log('Error with Add Balance POST saga', err)
+    }
+}
