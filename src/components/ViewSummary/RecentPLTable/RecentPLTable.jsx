939f6611e4d663d4adb9f48a20aa9fc82508e602
Adam Oberle <oberle.adam@gmail.com>
2023-08-01 12:50:59 -0500

    deleted old table from RecentPLTable component, added id to sql get query for recentPL table

--- src/components/ViewSummary/RecentPLTable/RecentPLTable.jsx
+++ src/components/ViewSummary/RecentPLTable/RecentPLTable.jsx
@@ -10,92 +10,11 @@ function RecentPLTable() {
 
     return (
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Most Recent Week</th>
-                    </tr>
-                    <tr>
-                        <th>Income</th>
-                    </tr>
-                    <tr>
-                        <th>Date</th>
-                        <th>Payee</th>
-                        <th>Amount</th>
-                        <th>Paid</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* using map to loop through recentPL variable */}
-                    {recentPL.map(transaction => {
-                        // for each transaction, if category id is equal to 1 (income) and paid is set to true
-                        // return this code
-                        if (transaction.category_id === 1 && transaction.paid === true) {
-                            return <tr key={transaction.id}>
-                                <td>{transaction.date}</td>
-                                <td>{transaction.payee}</td>
-                                <td>{transaction.amount}</td>
-                                <td><input type="checkbox" checked={transaction.paid} readOnly /></td>
-                            </tr>
-                            // otherwise if category is income, but paid is set to false, render this code
-                        } else if (transaction.category_id === 1 && transaction.paid === false) {
-                            return <tr key={transaction.id}>
-                                <td>{transaction.date}</td>
-                                <td>{transaction.payee}</td>
-                                <td>{transaction.amount}</td>
-                                <td><input type="checkbox" readOnly /></td>
-                            </tr>
-                        }
-                    })}
-                </tbody>
-                <tbody>
-
-                </tbody>
-            </table>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Most Recent Week</th>
-                    </tr>
-                    <tr>
-                        <th>Expenses</th>
-                    </tr>
-                    <tr>
-                        <th>Date</th>
-                        <th>Payee</th>
-                        <th>Amount</th>
-                        <th>Paid</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* using map to loop through recentPL variable */}
-                    {recentPL.map(transaction => {
-                        // for each transaction, if category id is equal to 2 (expense) and paid is set to true
-                        // return this code
-                        if (transaction.category_id === 2 && transaction.paid === true) {
-                            return <tr key={transaction.id}>
-                                <td>{transaction.date}</td>
-                                <td>{transaction.payee}</td>
-                                <td>{transaction.amount}</td>
-                                <td><input type="checkbox" checked={transaction.paid} readOnly /></td>
-                            </tr>
-                            // otherwise if category is expense, but paid is set to false, render this code
-                        } else if (transaction.category_id === 2 && transaction.paid === false) {
-                            return <tr key={transaction.id}>
-                                <td>{transaction.date}</td>
-                                <td>{transaction.payee}</td>
-                                <td>{transaction.amount}</td>
-                                <td><input type="checkbox" disabled /></td>
-                            </tr>
-                        }
-                    })}
-                </tbody>
-            </table>
             <TableContainer>
                 <Table aria-label="recent-PL">
                     <TableHead>
                         <TableRow align="center">
-                            <TableCell>Most Recent Week</TableCell>
+                            <TableCell>Most Recent Week Profit and Loss</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell
