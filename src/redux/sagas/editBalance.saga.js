c5fec618091bd67f2e69dad3142894a602002753
Majid Igale <majidigale18@gmail.com>
2023-08-01 12:09:15 -0500

    created edit feature for EditBalance saga

---
+++ src/redux/sagas/editBalance.saga.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import {put, takeLatest} from 'redux-saga/effects';
+
+
+function* editBalance(action){
+    try{
+        console.log('PUT EDIT BALANCE action.payload ===>', action.payload)
+        yield axios.put('/api/balance/edit', action.payload)
+        yield put({
+            type: 'FETCH_BALANCE'
+        })
+    } catch(err){
+        console.log('ERROR with CLIENT SAVING SAGA')
+    }
+}
+
+function* editBalanceSaga(){
+    yield takeLatest('EDIT_BALANCE', editBalance)
+}
+
+export default editBalance
