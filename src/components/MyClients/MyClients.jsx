a739695c4f1ef2e89c5c31cb2868cf593d0e2c3b
Will Richardson <rich1051@umn.edu>
2023-08-01 09:40:59 -0500

    MUI styling

--- src/components/MyClients/MyClients.jsx
+++ src/components/MyClients/MyClients.jsx
@@ -2,39 +2,43 @@ import React, { useEffect, useReact } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 import MyClientsItem from "./MyClientsItem/MyClientsItem";
-
+import { Typography, Button } from "@mui/material";
 
 function MyClients() {
-
-    const dispatch = useDispatch();
-    const myClients = useSelector((store) => store.myClients);
-    const history = useHistory()
+  const dispatch = useDispatch();
+  const myClients = useSelector((store) => store.myClients);
+  const history = useHistory();
 
   useEffect(() => {
     dispatch({
       type: "FETCH_MY_CLIENTS",
     });
-      dispatch({type:"UNSET_SELECTED_CLIENT"});
+    dispatch({ type: "UNSET_SELECTED_CLIENT" });
   }, []);
 
   const goClients = () => {
-    history.push('/clients')
-  }
+    history.push("/clients");
+  };
 
   // console.log('my clients list is:', myClients);
 
-        return (
-            <div>
-                <h2>Accountants's Clients</h2>
-            <ul>
-                {myClients && myClients.map((client, i) => (
-                  <MyClientsItem key={i} client={client}/>
-
-               ))}
-                
-            </ul>
-            <button onClick={goClients}>Clients</button>
-        </div>)
+  return (
+    <div>
+      <Typography variant="h2">Accountants's Clients</Typography>
+      {myClients && myClients.length > 0 ? (
+        <ul>
+          {myClients.map((client, i) => (
+            <MyClientsItem key={i} client={client} />
+          ))}
+        </ul>
+      ) : (
+        <Typography variant="body1">No clients found.</Typography>
+      )}
+      <Button variant="contained" onClick={goClients}>
+        Clients
+      </Button>
+    </div>
+  );
 }
 
-export default MyClients
+export default MyClients;
--- src/components/MyClients/MyClientsItem/MyClientsItem.jsx
+++ src/components/MyClients/MyClientsItem/MyClientsItem.jsx
@@ -1,28 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { Typography, Button } from "@mui/material";
 
-function MyClientsItem({client}) {
+function MyClientsItem({ client }) {
+  const path = `/viewsummary/${client.client_id}`;
+  const dispatch = useDispatch();
 
-    const path = `/viewsummary/${client.client_id}`
-    const dispatch = useDispatch();
+  console.log("oneClient is:", client);
 
-    console.log('oneClient is:', client);
+  const removeClient = (client_id) => {
+    dispatch({
+      type: "REMOVE_CLIENT",
+      payload: { client_id },
+    });
+    console.log("Assigned to Accountant");
+  };
 
-    const removeClient = (client_id) => {
-        dispatch({
-            type: 'REMOVE_CLIENT',
-            payload: {client_id}
-        })
-        console.log("Assigned to Accountant");   
-  }
-
-    return (
-        <div>
-            <li > {client.company_name}</li>
-            <Link to={path} >View Client Details</Link>
-            <button onClick={() => removeClient(client.client_id)}>❌</button>
-        </div>
-    )
+  return (
+    <div>
+      <Typography variant="body1">{client.company_name}</Typography>
+      <Link to={path}>View Client Details</Link>
+      <Button
+        variant="contained"
+        onClick={() => removeClient(client.client_id)}
+      >
+        Remove
+      </Button>
+    </div>
+  );
 }
 
-export default MyClientsItem
+export default MyClientsItem;
