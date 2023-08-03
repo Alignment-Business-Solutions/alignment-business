d18e9a720f01bc49fd9706157a46bd541607a5b7
Adam Oberle <oberle.adam@gmail.com>
2023-08-03 10:45:13 -0500

    made tables on allPL scrollable, moved edit week and export week buttons outside table

--- src/components/ViewSummary/RecentPLTable/RecentPLTable.jsx
+++ src/components/ViewSummary/RecentPLTable/RecentPLTable.jsx
@@ -16,9 +16,11 @@ function RecentPLTable() {
                     <TableHead>
                         <TableRow>
                             <TableCell
-                            sx={{textAlign: "center"}} 
-                            colSpan={4}
-                            >Income</TableCell>
+                                sx={{ textAlign: "center" }}
+                                colSpan={4}
+                            >
+                                Income
+                            </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Date</TableCell>
@@ -28,26 +30,26 @@ function RecentPLTable() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                    {recentPL.map(transaction => {
-                        // for each transaction, if category id is equal to 2 (expense) and paid is set to true
-                        // return this code
-                        if (transaction.category_id === 1 && transaction.paid === true) {
-                            return <TableRow key={transaction.id}>
-                                <TableCell>{transaction.date}</TableCell>
-                                <TableCell>{transaction.payee}</TableCell>
-                                <TableCell>{transaction.amount}</TableCell>
-                                <TableCell><Checkbox checked={transaction.paid} readOnly /></TableCell>
-                            </TableRow>
-                            // otherwise if category is expense, but paid is set to false, render this code
-                        } else if (transaction.category_id === 1 && transaction.paid === false) {
-                            return <TableRow key={transaction.id}>
-                                <TableCell>{transaction.date}</TableCell>
-                                <TableCell>{transaction.payee}</TableCell>
-                                <TableCell>{transaction.amount}</TableCell>
-                                <TableCell><Checkbox disabled /></TableCell>
-                            </TableRow>
-                        }
-                    })}
+                        {recentPL.map(transaction => {
+                            // for each transaction, if category id is equal to 2 (expense) and paid is set to true
+                            // return this code
+                            if (transaction.category_id === 1 && transaction.paid === true) {
+                                return <TableRow key={transaction.id}>
+                                    <TableCell>{transaction.date}</TableCell>
+                                    <TableCell>{transaction.payee}</TableCell>
+                                    <TableCell>{transaction.amount}</TableCell>
+                                    <TableCell><Checkbox checked={transaction.paid} readOnly /></TableCell>
+                                </TableRow>
+                                // otherwise if category is expense, but paid is set to false, render this code
+                            } else if (transaction.category_id === 1 && transaction.paid === false) {
+                                return <TableRow key={transaction.id}>
+                                    <TableCell>{transaction.date}</TableCell>
+                                    <TableCell>{transaction.payee}</TableCell>
+                                    <TableCell>{transaction.amount}</TableCell>
+                                    <TableCell><Checkbox disabled /></TableCell>
+                                </TableRow>
+                            }
+                        })}
                     </TableBody>
                 </Table>
             </TableContainer>
@@ -55,10 +57,15 @@ function RecentPLTable() {
                 <Table aria-label="recent-PL">
                     <TableHead>
                         <TableRow>
-                            <TableCell sx={{borderBottom: "none"}}></TableCell>
+                            <TableCell sx={{ borderBottom: "none" }}></TableCell>
                         </TableRow>
                         <TableRow>
-                            <TableCell sx={{textAlign: "center"}} colSpan={4}>Expenses</TableCell>
+                            <TableCell
+                                sx={{ textAlign: "center" }}
+                                colSpan={4}
+                            >
+                                Expenses
+                            </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Date</TableCell>
@@ -68,26 +75,26 @@ function RecentPLTable() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                    {recentPL.map(transaction => {
-                        // for each transaction, if category id is equal to 2 (expense) and paid is set to true
-                        // return this code
-                        if (transaction.category_id === 2 && transaction.paid === true) {
-                            return <TableRow key={transaction.id}>
-                                <TableCell>{transaction.date}</TableCell>
-                                <TableCell>{transaction.payee}</TableCell>
-                                <TableCell>{transaction.amount}</TableCell>
-                                <TableCell><Checkbox checked={transaction.paid} readOnly /></TableCell>
-                            </TableRow>
-                            // otherwise if category is expense, but paid is set to false, render this code
-                        } else if (transaction.category_id === 2 && transaction.paid === false) {
-                            return <TableRow key={transaction.id}>
-                                <TableCell>{transaction.date}</TableCell>
-                                <TableCell>{transaction.payee}</TableCell>
-                                <TableCell>{transaction.amount}</TableCell>
-                                <TableCell><Checkbox type="checkbox" disabled /></TableCell>
-                            </TableRow>
-                        }
-                    })}
+                        {recentPL.map(transaction => {
+                            // for each transaction, if category id is equal to 2 (expense) and paid is set to true
+                            // return this code
+                            if (transaction.category_id === 2 && transaction.paid === true) {
+                                return <TableRow key={transaction.id}>
+                                    <TableCell>{transaction.date}</TableCell>
+                                    <TableCell>{transaction.payee}</TableCell>
+                                    <TableCell>{transaction.amount}</TableCell>
+                                    <TableCell><Checkbox checked={transaction.paid} readOnly /></TableCell>
+                                </TableRow>
+                                // otherwise if category is expense, but paid is set to false, render this code
+                            } else if (transaction.category_id === 2 && transaction.paid === false) {
+                                return <TableRow key={transaction.id}>
+                                    <TableCell>{transaction.date}</TableCell>
+                                    <TableCell>{transaction.payee}</TableCell>
+                                    <TableCell>{transaction.amount}</TableCell>
+                                    <TableCell><Checkbox type="checkbox" disabled /></TableCell>
+                                </TableRow>
+                            }
+                        })}
                     </TableBody>
                 </Table>
             </TableContainer>
