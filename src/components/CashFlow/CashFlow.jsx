f22d05c015358d2cc84e9cebbcf3fda00f07b2a4
Adam Oberle <oberle.adam@gmail.com>
2023-08-03 16:28:54 -0500

    getting balance sheet information for viewSummary page

---
+++ src/components/CashFlow/CashFlow.jsx
@@ -0,0 +1,15 @@
+import { LineChart } from '@mui/x-charts';
+
+
+function CashFlow() {
+
+    return (
+        <div>
+            {/* <LineChart>
+
+            </LineChart> */}
+        </div>
+    )
+}
+
+export default CashFlow;
--- src/components/ViewSummary/ViewSummary.jsx
+++ src/components/ViewSummary/ViewSummary.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import { Box } from "@mui/material";
+import { Box, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
 import AddWeekForm from "./AddWeekForm/AddWeekForm";
 import RecentPLTable from "./RecentPLTable/RecentPLTable";
@@ -15,10 +16,16 @@ function ViewSummary() {
     const client_id = useParams();
     const params = useParams();
     const clientID = params.client_id;
+    const recentBalance = useSelector(store => store.recentBalance);
+
+    console.log('Recent balance on viewSummary is:', recentBalance);
+
 
     function pageLoad() {
         // dispatching to fetch_recent_pl.saga.js
         dispatch({ type: 'FETCH_RECENT_PL', payload: {client: clientID} });
+
+        dispatch({ type: 'FETCH_RECENT_BALANCE', payload: {client: clientID}});
         // dispatching to fetchWeeksDropdown.saga.js
         dispatch({ type: 'FETCH_WEEKS_DROPDOWN'});
         // dispatching to clients.saga.js
@@ -35,6 +42,37 @@ function ViewSummary() {
             <p className="center-text">Select a Week from the menu to view/edit details!</p>
             <br/>
             <SelectWeekForm />
+            <br/>
+            <Box align="center">
+                <p>Most Recent Balance</p>
+                <Card variant="outlined"
+                    style={{
+                      backgroundColor: "#00B4EB",
+                      color: "white",
+                      width: 200,
+                    }}>
+                    <CardContent>
+                      <Typography sx={{ fontSize: "1.5rem" }}>
+                        Week Start Date
+                      </Typography>
+                      <Typography>
+                        {recentBalance.start_date}
+                      </Typography>
+                      <Typography sx={{ fontSize: "1.2rem" }}>
+                        Beginning Cash
+                      </Typography>
+                      <Typography>
+                        {recentBalance.beginning_cash}
+                      </Typography>
+                      <Typography sx={{ fontSize: "1.2rem" }}>
+                        Ending Balance
+                      </Typography>
+                      <Typography>
+                        {recentBalance.ending_balance_actual}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+            </Box>
             <br/>
             <AddWeekForm />
             <br/>
